734088c1f8ceabfa5428132f3f0dc921e0676339
Unknown <mikeblack25@gmail.com>
2019-04-14 10:39:12 -0700

    Update Version

--- Bromines/Properties/AssemblyInfo.cs
+++ Bromines/Properties/AssemblyInfo.cs
@@ -7,5 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb346a0f-1dd9-49ef-8d1f-da87b91963cd")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
