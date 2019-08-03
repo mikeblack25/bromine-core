c71e99c78460210f00c4dc308fea2e99448fe355
Mike Black <mikeblack25@gmail.com>
2019-08-03 08:08:30 -0700

    Project naming updates
    - Rename The solution and Bromine.csproj to BromineCore.
    - Update project file relative paths to use $(SolutionDir).

--- Bromine/Properties/AssemblyInfo.cs
+++ Bromine/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Bromine")]
-[assembly: AssemblyProduct("Bromine")]
+[assembly: AssemblyTitle("BromineCore")]
+[assembly: AssemblyProduct("Bromine Core")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bb346a0f-1dd9-49ef-8d1f-da87b91963cd")]
