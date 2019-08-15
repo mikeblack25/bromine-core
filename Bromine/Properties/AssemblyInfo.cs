e3108b2e27e194b50dbb538d6b4c15576acab43d
Mike Black <mikeblack25@gmail.com>
2019-08-14 22:28:20 -0700

    Update version to 1.0.0.2

--- Bromine/Properties/AssemblyInfo.cs
+++ Bromine/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("bb346a0f-1dd9-49ef-8d1f-da87b91963cd")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 
 [assembly: InternalsVisibleTo("Tests.Bromine")]
