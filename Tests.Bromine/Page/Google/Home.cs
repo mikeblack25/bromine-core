7da1c48f5afe138158bbba2d532b59f4eee151c3
Mike Black <mikeblack25@gmail.com>
2019-09-03 22:45:20 -0700

    Add POM Example for Google Homepage
    - Add Page class to Bromine.Core as a base for all Page Objects to inherit from.
    - Add Home as an example of a simple Page Object.
    - Add HomeTests to show a simple usage with the Home Page Object, PageBase, and Page.
    - Add PageBase as an example of a TestFixture to expose all necessary information for tests.

---
+++ Tests.Bromine/Page/Google/Home.cs
@@ -0,0 +1,52 @@
+﻿using Bromine.Core;
+using Bromine.Core.ElementInteraction;
+
+using OpenQA.Selenium;
+
+namespace Tests.Bromine.Page.Google
+{
+    /// <summary>
+    /// Representation of the Google Homepage https://www.google.com.
+    /// </summary>
+    public class Home : global::Bromine.Core.Page
+    {
+        /// <inheritdoc />
+        public Home(Browser browser) : base(browser)
+        {
+        }
+
+#pragma warning disable 1591
+        public Element AboutLink => Browser.SeleniumFind.ElementByText("About");
+        public Element StoreLink => Browser.SeleniumFind.ElementByText("Store");
+        public Element GmailLink => Browser.SeleniumFind.ElementByText("Gmail");
+        public Element ImagesLink => Browser.SeleniumFind.ElementByText("Images");
+        public Element GoogleAppsLink => Browser.Find.ElementByClasses("gb_z", "gb_uc");
+        public Element ProfileLink => Browser.Find.ElementByClasses("gb_z", "gb_Ia", "gb_g");
+        public Element GoogleImage => Browser.Find.Element("#hplogo");
+        public Element SearchField => Browser.Find.ElementByClasses("gLFyf", "gsfi");
+        public Element GoogleSearchButton => Browser.Find.Element("[value='Google Search']");
+        public Element RandomSearchButton => Browser.Find.Element("center > div > div > div:nth-child(2) > span");
+        public Element AdvertisingLink => Browser.SeleniumFind.ElementByText("Advertising");
+        public Element BusinessLink => Browser.SeleniumFind.ElementByText("Business");
+        public Element HowSearchWorksLink => Browser.SeleniumFind.ElementByText("How Search works");
+        public Element PrivacyLink => Browser.SeleniumFind.ElementByText("Privacy");
+        public Element TermsLink => Browser.SeleniumFind.ElementByText("Terms");
+        public Element SettingsLink => Browser.SeleniumFind.ElementByText("Settings");
+#pragma warning restore 1591
+
+        /// <summary>
+        /// search for the given searchTerm.
+        /// </summary>
+        /// <param name="searchTerm">Term to search for on Google.</param>
+        public void Search(string searchTerm)
+        {
+            SearchField.SendKeys(searchTerm);
+            SearchField.SendKeys(Keys.Enter);
+        }
+
+        /// <summary>
+        /// https://www.google.com/
+        /// </summary>
+        public override string Url => "https://www.google.com/";
+    }
+}
